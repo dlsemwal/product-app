416404e44f7969b451876ebb6914ca299cf65cb1
Divyanshu Semwal <dlsemwal@gmail.com>
2019-05-10 18:22:15 +0530

    add-product component created

--- product-frintend/src/app/app.module.ts
+++ product-frintend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'add-product', component: AddComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
